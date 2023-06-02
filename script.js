a410071f49dd66211bc767784993adad645678c6
Picasso <urvishp13@gmail.com>
2023-06-02 12:12:41 -0400

    added functionality to RECORD button

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // grab count-el from document to be able to change its value
 const countEl = document.getElementById("count-el")
+const recordEl = document.getElementById("record-el")
 
-console.log(countEl)
+console.log(recordEl)
 
 // create and initialize variable count so can use in functions
 let count = 0
 
-// add functionality to increment function
+// add functionality to INCREMENT button
 function increment() {
     // console.log("increment button clicked")
     count += 1
     countEl.textContent = count
+}
+
+// add functionality to RECORD button
+    // make it so that it saves the counted (incremented) value to recordEl
+function record() {
+    recordEl.textContent += " " + count + ", "
+    // reset count and countEl to 0 to start anew for next counting
+    count = 0
+    countEl.textContent = 0
 }
