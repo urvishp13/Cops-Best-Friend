cf7d857bb64440ef6b33f7697b39fa7792fcf6e2
Picasso <urvishp13@gmail.com>
2023-06-03 16:10:35 -0400

    added page redirect

--- resources/js/timer.js
+++ resources/js/timer.js
@@ -2,7 +2,7 @@
 const timerEl = document.getElementById("timer-el")
 
 // add functionality to the timer
-setInterval(timer, 1000)
+const timerInterval = setInterval(timer, 1000)
 
 function timer() {
     // get the current second relative to real-time
@@ -14,6 +14,14 @@ function timer() {
     timerEl.textContent = 60 - currentSecond
     // when the timer hits 0, reset countEl and count (the timer gets reset to 1min automatically)
     if (currentSecond === 0) {
-        recordEl.textContent = " "
+        reset() // refresh/revert to index.html page
     }
+}
+
+function reset() {
+    let meta = document.createElement("meta")
+    meta.httpEquiv = "refresh"
+    meta.content = "1; URL=index.html"
+    document.getElementsByTagName("head")[0].appendChild(meta)
+    clearInterval(timerInterval) // stop timer after hit zero seconds
 }
